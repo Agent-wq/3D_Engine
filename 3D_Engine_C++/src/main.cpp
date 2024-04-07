e0f78138ecc35316cef4959b9a4feaf6f88e4f53
Aadil <adilshigan20@gmail.com>
2024-04-07 13:47:10 +0530

    added

--- 3D_Engine_C++/src/main.cpp
+++ 3D_Engine_C++/src/main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "stb_image.h"
 #include <shader.h>
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
